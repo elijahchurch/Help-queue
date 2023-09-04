f6ce48dfae5fa5ab8803340ebe89dfb411e3085f
Jason E. Church <elijahchurch@gmail.com>
2023-09-03 17:55:32 -0700

    Build basic App function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const name = "Thato";
+    const name2 = "Haley";
+    return(
+        <React.Fragment>
+            <h1>Help Queue</h1>
+            <h3>3a</h3>
+            <h3>{name} and {name2}</h3>
+            <p><em>Firebase entries not saving!</em></p>
+            {/* JSX comment example */}
+            <hr/>
+        </React.Fragment>
+    );
 }
 
 export default App;
