f6349382a412be85db43aaa52623330fcfd3b4a4
Jason E. Church <elijahchurch@gmail.com>
2023-09-10 18:40:24 -0700

    Create DELETE_TICKET action and pass test

--- src/_tests_/reducers/ticket-list-reducer.test.js
+++ src/_tests_/reducers/ticket-list-reducer.test.js
@@ -10,15 +10,29 @@ describe("ticketListReducer", () => {
         id: 1
     };
 
+    const currentState = {
+        1: {
+            names: 'Ryan & Aimen',
+            location: '4b',
+            issue: 'Redux action is not working correctly.',
+            id: 1
+        }, 2: {
+            names: 'Jasmine and Justine',
+            location: '2a',
+            issue: 'Reducer has side effects.',
+            id: 2
+        }
+    }
+
 
     test("Should return default state if there is no action type passed into the reducer", () => {
-        expect(ticketListReducer({}, {type: null})).toEqual({});
+        expect(ticketListReducer({}, { type: null })).toEqual({});
     });
 
     test("Should successfully add new ticket data to mainTicketList", () => {
-        const { names, location , issue , id} = ticketData;
+        const { names, location, issue, id } = ticketData;
         action = {
-            type: "ADD_TICKET" , 
+            type: "ADD_TICKET",
             names: names,
             location: location,
             issue: issue,
@@ -26,7 +40,7 @@ describe("ticketListReducer", () => {
         };
 
         expect(ticketListReducer({}, action)).toEqual({
-            [id] : {
+            [id]: {
                 names: names,
                 location: location,
                 issue: issue,
@@ -35,5 +49,22 @@ describe("ticketListReducer", () => {
         });
     });
 
-    
+    test("Should successfully delete a ticket", () => {
+        action = {
+            type: "DELETE_TICKET",
+            id: 1
+        };
+        expect(ticketListReducer(currentState, action)).toEqual({
+            2: {
+                names: "Jasmine and Justine",
+                location: "2a",
+                issue: "Reducer has side effects.",
+                id: 2
+            }
+        });
+    });
+
+
+
+
 });
